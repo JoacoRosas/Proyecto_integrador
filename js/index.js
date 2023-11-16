df09fb3affdae6f1e9e68dd04504cfff041d311d
VilaMariano <vilam@udesa.edu.ar>
2023-11-16 20:09:03 -0300

    Hice el punto 3

--- js/index.js
+++ js/index.js
@@ -12,12 +12,13 @@ fetch(urlPeliculas)
         let arrayPeliculas = data.results
         let peliculasHTML = ''
 
-        for(let i = 0; i <arrayPeliculas.length; i++){
-            peliculasHTML =+ `<li>
+        for(let i = 0; i < 10; i++){
+            peliculasHTML += `<li>
             <article class = "article-lista-movie">
             <a class="enlace-lista-movie" href="detalle-movie.html?id=${arrayPeliculas[i].id}">
-            <h2 class="titulo-lista-movie"></h2>
-            <img src= 
+            <h2 class="titulo-lista-movie"> ${arrayPeliculas[i].title} </h2>
+            <img src="https://image.tmdb.org/t/p/w500${arrayPeliculas[i].poster_path}" width="400" height="600" alt="${arrayPeliculas[i].title} Poster">
+            <p>${arrayPeliculas[i].release_date}</p>
             </a>
             </article>
             </li>`
@@ -28,6 +29,43 @@ fetch(urlPeliculas)
         console.log("Error: " + error);
     }) 
 
+
+///SERIES///
+let urlSeries = `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
+
+fetch(urlSeries)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+
+        let series = document.querySelector('.lista-series')
+        let arraySeries = data.results
+        let seriesHTML = ''
+
+        for(let i = 0; i < 5; i++){
+            seriesHTML += `<li>
+            <article class = "article-lista-movie">
+            <a class="enlace-lista-movie" href="detalle-serie.html?id=${arraySeries[i].id}">
+            <h2 class="titulo-lista-movie"> ${arraySeries[i].original_name } </h2>
+            <img src="https://image.tmdb.org/t/p/w500${arraySeries[i].poster_path}" width="400" height="600" alt="${arraySeries[i].original_name} Poster">
+            <p>${arraySeries[i].first_air_date}</p>
+            </a>
+            </article>
+            </li>`
+        }
+        series.innerHTML = seriesHTML;
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    }) 
+
+
+
+
+
+///BUSCADOR///
 
 
 let formulario = document.querySelector(".formulario")
